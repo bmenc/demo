56dcbf636f15d362e31f4e0f553b2f38d9f1f079
bmenchaca <bmenchaca.cca@gmail.com>
2025-01-24 15:11:41 -0600

    feat: Implement login page with form validation and UI components; add new input and label components

---
+++ app/(logged-out)/login/page.tsx
@@ -0,0 +1,91 @@
+"use client";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { PanelsTopLeft } from "lucide-react";
+import { useForm } from "react-hook-form";
+import * as zod from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = zod.object({
+  email: zod.string().email(),
+  password: zod.string(),
+});
+
+export default function LoginPage() {
+  const form = useForm<zod.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function handleSubmit(data: zod.infer<typeof formSchema>) {
+    console.log(data);
+  }
+
+  return (
+    <>
+      <PanelsTopLeft size={40} strokeWidth={2} className="text-slate-700" />
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+          <CardDescription>Login to your Dashboard account</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(handleSubmit)}>
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => {
+                  return (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="john@doe.com"
+                          type="email"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormDescription>
+                        This is the email address you signed up to Dashboard
+                        with
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  );
+                }}
+              ></FormField>
+            </form>
+          </Form>
+        </CardContent>
+        <CardFooter className="justify-between">
+          <small>Don&apos;t have an account?</small>
+          <Button asChild variant="outline" size="sm">
+            <Link href="/sign-up">Sign up</Link>
+          </Button>
+        </CardFooter>
+      </Card>
+    </>
+  );
+}
