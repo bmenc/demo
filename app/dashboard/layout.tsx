b339d1f8b218bc7b691ff3bc25783216e144330b
bmenc <bmenchaca.cca@gmail.com>
2025-02-13 15:49:32 -0600

    feat: add tabs component and employee card statistics to dashboard page

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -3,7 +3,7 @@ import MainMenu from "@/app/dashboard/components/main-menu";
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="grid grid-cols-[250px_1fr] h-screen">
+    <div className="grid grid-cols-[250px_1fr] h-screen w-screen">
       <MainMenu />
       <div className="overflow-auto py-2 px-4">
         <h1 className="pb-4">Welcome back</h1>
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,17 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import EmployeesStats from "./components/employees-stats";
+
 export default function DashboardPage() {
-  return <div>Dashboard Page</div>;
+  return (
+    <Tabs defaultValue="employees" className="w-full">
+      <TabsList className="mb-4">
+        <TabsTrigger value="employees">Employees</TabsTrigger>
+        <TabsTrigger value="teams">Teams stats</TabsTrigger>
+      </TabsList>
+      <TabsContent value="employees">
+        <EmployeesStats />
+      </TabsContent>
+      <TabsContent value="teams">teams stats view</TabsContent>
+    </Tabs>
+  );
 }
