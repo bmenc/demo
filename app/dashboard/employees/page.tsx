9ca3c99bcf0f1a53d5bdd085916ad7579b3b8782
bmenc <bmenchaca.cca@gmail.com>
2025-02-16 03:18:20 -0600

    fix: move employee data to separate file

--- app/dashboard/employees/page.tsx
+++ app/dashboard/employees/page.tsx
@@ -1,76 +1,8 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { DataTable } from "@/components/ui/data-table";
 import { setTimeout } from "timers/promises";
-import { columns, type Employee } from "./columns";
-
-export const employees: Employee[] = [
-  {
-    id: 1,
-    firstName: "Colin",
-    lastName: "Murray",
-    teamName: "alpha",
-    isTeamLeader: true,
-    avatar: "/images/cm.jpg",
-  },
-  {
-    id: 2,
-    firstName: "Tom",
-    lastName: "Phillips",
-    teamName: "alpha",
-    isTeamLeader: false,
-  },
-  {
-    id: 3,
-    firstName: "Liam",
-    lastName: "Fuentes",
-    teamName: "alpha",
-    isTeamLeader: false,
-  },
-  {
-    id: 4,
-    firstName: "Tina",
-    lastName: "Fey",
-    teamName: "canary",
-    isTeamLeader: true,
-    avatar: "/images/tf.jpg",
-  },
-  {
-    id: 5,
-    firstName: "Katie",
-    lastName: "Johnson",
-    teamName: "canary",
-    isTeamLeader: false,
-  },
-  {
-    id: 6,
-    firstName: "Tina",
-    lastName: "Jones",
-    teamName: "canary",
-    isTeamLeader: false,
-  },
-  {
-    id: 7,
-    firstName: "Amy",
-    lastName: "Adams",
-    teamName: "delta",
-    isTeamLeader: true,
-  },
-  {
-    id: 8,
-    firstName: "Ryan",
-    lastName: "Lopez",
-    teamName: "delta",
-    isTeamLeader: false,
-    avatar: "/images/rl.jpg",
-  },
-  {
-    id: 9,
-    firstName: "Jenny",
-    lastName: "Jones",
-    teamName: "delta",
-    isTeamLeader: false,
-  },
-];
+import { columns } from "./columns";
+import { employees } from "./employeesData";
 
 export default async function EmployeesPage() {
   await setTimeout(5000);
